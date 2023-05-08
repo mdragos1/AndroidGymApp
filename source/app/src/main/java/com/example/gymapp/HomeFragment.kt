1e26e2f15391f59e7cb0ec7714fd3fe9f638776c
Dragos Stefan Mihalcea <dragos.mihalcea@s.unibuc.ro>
2023-05-08 20:05:52 +0300

    Implemented basic video playback

--- source/app/src/main/java/com/example/gymapp/HomeFragment.kt
+++ source/app/src/main/java/com/example/gymapp/HomeFragment.kt
@@ -1,10 +1,15 @@
 package com.example.gymapp
 
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.MediaController
+import android.widget.Toast
+import android.widget.VideoView
 import com.example.gymapp.databinding.FragmentHomeBinding
 
 // TODO: Rename parameter arguments, choose names that match
@@ -21,6 +26,9 @@ class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private var videoView: VideoView? = null
+    private var mediaController : MediaController? = null
 
     private lateinit var binding: FragmentHomeBinding
 
@@ -39,6 +47,8 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+        videoView = binding.videoView
+        setUpVideoPlayer()
         return binding.root
     }
 
@@ -60,5 +70,31 @@ class HomeFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    private fun setUpVideoPlayer(){
+        if (mediaController == null){
+            mediaController = MediaController(this.context)
+            mediaController!!.setAnchorView(this.videoView)
+        }
+        videoView!!.setMediaController(mediaController)
+
+        videoView!!.setVideoURI(
+            Uri.parse("android.resource://"
+                    + "com.example.gymapp" + "/" + R.raw.intro)
+        )
+
+        videoView!!.requestFocus()
+
+        videoView!!.pause()
+
+        videoView!!.setOnCompletionListener {
+            Toast.makeText(context, "Video Completed", Toast.LENGTH_SHORT).show()
+        }
+
+        videoView!!.setOnErrorListener { mp, what, extra ->
+            Toast.makeText(context, "An Error Occurred", Toast.LENGTH_SHORT).show()
+            false
+        }
     }
 }
